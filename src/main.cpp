e36c1f3ba9ad8292843b0693926389d27174af3e
Kate_nguyen <253353253+MeoMeoDestroyer@users.noreply.github.com>
2026-02-24 05:04:59 +0000

    add yspeed and update

--- src/main.cpp
+++ src/main.cpp
@@ -14,6 +14,12 @@ static constexpr int HALF_SCREEN_WIDTH = bn::display::width() / 2;
 static constexpr bn::fixed MIN_X = -HALF_SCREEN_WIDTH;
 static constexpr bn::fixed MAX_X = HALF_SCREEN_WIDTH;
 
+// Set max/min y position to be the edges of the display
+static constexpr int HALF_SCREEN_HEIGHT = bn::display::height() / 2;
+static constexpr bn::fixed MIN_Y = -HALF_SCREEN_HEIGHT;
+static constexpr bn::fixed MAX_Y = HALF_SCREEN_HEIGHT;+
+
+
 // Starting speed of a bouncer
 static constexpr bn::fixed BASE_SPEED = 2;
 
@@ -23,13 +29,19 @@ static constexpr int MAX_BOUNCERS = 20;
 class Bouncer {
     public:
         bn::sprite_ptr sprite = bn::sprite_items::dot.create_sprite();
+
         bn::fixed x_speed = BASE_SPEED;
+        bn::fixed y_speed = BASE_SPEED;
 
     void update() {
         bn::fixed x = sprite.x();
+        bn::fixed y = sprite.y();
 
         // Update x position by adding speed
         x += x_speed;
+
+        // Update y position by adding speed
+        y += y_speed;
 
         // If we've gone off the screen on the right
         if(x > MAX_X) {
@@ -44,7 +56,21 @@ class Bouncer {
             x_speed *= -1;
         }
 
+        // If we've gone off the screen on the bottom
+        if(y > MAX_Y) {
+            // Snap back to screen and reverse direction
+            y = MAX_Y;
+            y_speed *= -1;
+        }
+        // If we've gone off the screen on the top
+        if(y < MIN_Y) {
+            // Snap back to screen and reverse direction
+            y = MIN_Y;
+            y_speed *= -1;
+        }
+
         sprite.set_x(x);
+        sprite.set_y(y);
     }
 };
 
